438938fbdc56dccf3394829fc41c58c5adf7af64
Jeb Henrie <jebhenrie@gmail.com>
2018-08-22 18:15:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes = {
+  prop: 1,
+  prop2: 2
+  };
+function updateObjectWithKeyAndValue(object,key,value){
+  object[key] = value
+  return object
+}
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
+  }
+  
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  const newobj =
+Object.assign({},object)
+  delete newobj.prop
+  return newobj
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete recipes.prop
+  return recipes
+}
